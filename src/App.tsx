10a9a38b0e8095ad32233ec1f8f3ed8b3292ebbf
muzzHussain <mailtomuzz@gmail.com>
2024-01-16 21:28:22 +0530

    chip component done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import {SearchUser} from './Components/SearchUser.jsx';
+
+function App() {
+  return (
+    <>
+      <SearchUser/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/SearchUser.tsx
@@ -0,0 +1,157 @@
+import {useState, useEffect, useRef} from "react";
+import userData from '../Data/userData.json'
+import { User } from "../userInterface";
+
+export const SearchUser = () => {
+
+    const [isInputFocused, setIsInputFocused] = useState(true);
+    // const [selectedUser, setSelectedUser] = useState<User | null>(null);
+    const [selectedUsers, setSelectedUsers] = useState<User[]>([]);
+    const [availableUsers, setAvailableUsers] = useState<User[]>(userData);
+    const [isFirstBackspacePress, setIsFirstBackspacePress] = useState(true);
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        const handleBackspace = (event: KeyboardEvent) => {
+            if (event.key === 'Backspace' && inputRef.current && inputRef.current.value === '') {
+                const lastSelectedUser = selectedUsers[selectedUsers.length - 1];
+                if (lastSelectedUser) {
+                    if (isFirstBackspacePress) {
+                        setIsFirstBackspacePress(false);
+                        inputRef.current.blur();
+                        setSelectedUsers((prevSelectedUsers) => [...prevSelectedUsers]);
+                    } else {
+                        setIsFirstBackspacePress(true);
+                        setSelectedUsers((prevSelectedUsers) => prevSelectedUsers.slice(0, -1));
+                        setAvailableUsers((prevAvailableUsers) => [...prevAvailableUsers, lastSelectedUser]);
+                        inputRef.current.focus();
+                    }
+                }
+            }
+        };
+
+        window.addEventListener('keydown', handleBackspace);
+
+        return () => {
+            window.removeEventListener('keydown', handleBackspace);
+        };
+    }, [selectedUsers, isFirstBackspacePress]);
+
+    const handleUserClick = (user: User) => {
+        console.log('User', user);
+        setSelectedUsers((prevSelectedUsers) => {
+            console.log('Prev user', prevSelectedUsers)
+            // Remove the user from the list
+            const updatedUsers = prevSelectedUsers.filter((u) => u.id !== user.id);
+            // Add the selected user to the list
+            const newSelectedUsers = [...updatedUsers, user];
+            console.log('Updated', updatedUsers)
+            console.log('new selected users', newSelectedUsers)
+            setAvailableUsers((prevAvailableUsers) => prevAvailableUsers.filter((u) => u.id !== user.id));
+            setIsInputFocused(true);
+            return newSelectedUsers;
+        });
+    };
+    
+    // working
+    const handleClearSelection = (user: User) => {
+        setSelectedUsers((prevSelectedUsers) => prevSelectedUsers.filter((u) => u.id !== user.id));
+        setAvailableUsers((prevAvailableUsers) => [...prevAvailableUsers, user]);
+        setIsInputFocused(true);
+        
+        if (selectedUsers.length === 1) {
+            setIsInputFocused(false);
+        } else {
+            setIsInputFocused(true);
+            if (inputRef.current) {
+                inputRef.current.focus();
+            }
+        }
+    };
+
+
+    
+
+
+    // const handleUserClick = (user:User) => {
+    //     console.log('user', user);
+    //     setSelectedUser(user);
+    //     setIsInputFocused(false);
+
+    // };
+
+    // const handleClearSelection = () => {
+    //     setSelectedUser(null);
+    //     // console.log('Clear User', user);
+    //     setIsInputFocused(true);
+    // }
+
+    const handleInputBlur = () => {
+        setTimeout(() => {
+            if(selectedUsers.length === 0) 
+                setIsInputFocused(false)
+        }, 0)
+    }
+
+
+    return (
+       <div>
+            <div className="heading">
+                <h2>Pick Users</h2>
+            </div>
+            <div className="searchUser">
+                <div className="inputContainer">
+                    <input 
+                        type="text" 
+                        placeholder="Add new user" 
+                        onFocus={()=>setIsInputFocused(true)}
+                        onBlur={handleInputBlur}
+                        // readOnly
+                        ref={inputRef}
+                    />
+                    <div className="selectedUsersContainer">
+                        {selectedUsers.map((user, index) => (
+                            <div 
+                                key={user.id} 
+                                // className="selectedUser"
+                                className={`selectedUser ${index === selectedUsers.length - 1 ? 'highlight' : ''}`}
+                                >
+                                <img className="selectedUserImage" src={user.image} alt={user.name} />
+                                <span className="selectedUserName">{user.name}</span>
+                                <span className="clearSelection" onClick={() => handleClearSelection(user)}>X</span>
+                            </div>
+                        ))}
+                    </div>
+                    {/* {
+                    selectedUser && (
+                        <div className="selectedUser">
+                            <img className="selectedUserImage" src={selectedUser.image} alt={selectedUser.name} />
+                            <span className="selectedUserName">{selectedUser.name}</span>
+                            <span className="clearSelection" onClick={handleClearSelection}>X</span>
+
+                        </div>
+                    ) 
+                    
+                    }  */}
+                </div>
+            </div>
+                {
+                    isInputFocused && (
+                    <div className="userList">
+                        {availableUsers.map((user) => (
+                            <div 
+                            key={user.id} 
+                            className="userListItem"
+                            onClick={() => handleUserClick(user)}
+                            >
+                                <img src={user.image} alt={user.name} />
+                                <h5>{user.name}</h5>
+                                <p>{user.email}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+       </div>
+    );
+}
